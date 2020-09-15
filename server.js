daaec1ceb5f17614e7a6e74250da6b32f1b0a439
bassant <bassant.ayman69@gmail.com>
2020-09-15 17:33:39 +0300

    add package.json and install the required packages

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
+//require env
+require("dotenv").config();
+
+//import packages
+const express = require("express"),
+  bodyParser = require("body-parser");
+
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
 // Require Express to run server and routes
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -13,7 +21,10 @@ app.use(bodyParser.json());
 // Cors for cross origin allowance
 
 // Initialize the main project folder
-app.use(express.static('website'));
-
+app.use(express.static("website"));
 
 // Setup Server
+//start the server
+app.listen(process.env.PORT, () =>
+  console.log(`Server started on port ${process.env.PORT}`)
+);
